75a8292abf075885bc11b059c5137b23c8664c67
Anthony Fairchild <anthony@Anthonys-MBP.home>
2016-12-22 13:07:54 -0500

    longcat WIP

--- src/scene_clj/examples/longcat/core.clj
+++ src/scene_clj/examples/longcat/core.clj
@@ -1,10 +1,15 @@
 (ns scene-clj.examples.longcat.core
-  (:require [scene-clj.core :refer [run-scene scene]]
+  (:require [scene-clj.core :refer [run-scene scene assets]]
             [scene-clj.drawing :as d]
             [scene-clj.behavior :as b]))
 
 (def screen-size [1024 748])
 (def title "Longcat")
+
+(reset! assets [{:type :texture
+                 :file "images/head.png"}])
+
+(reset! scene [])
 
 (defn -main
   [& args]
